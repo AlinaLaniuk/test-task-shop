6bfb2d9473f1c1874a8f68a4bcb5aae13d60eeb0
Alina Laniuk <alinavoronina16@gmail.com>
2026-03-10 14:20:59 +0300

    feat: global styles

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import "../styles/globals.scss";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="container">{children}</div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import RootLayout from "./layout";
+
+export default function Main() {
+  return (
+    <RootLayout>
+      <div></div>
+    </RootLayout>
+  );
+}
