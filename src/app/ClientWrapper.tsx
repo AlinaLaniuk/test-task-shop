bc3fc11e66261a4aeee24777547727baf023a328
Alina Laniuk <alinavoronina16@gmail.com>
2026-03-11 18:36:23 +0300

    refactor: rootlayout

---
+++ src/app/ClientWrapper.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useUserStore } from "@/store/useUserStore";
+import useAuth from "@/hooks/useAuth";
+import Loader from "@/components/Loader/Loader";
+import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary";
+import RootError from "@/components/RootError/RootError";
+import { useErrorStore } from "@/store/useErrorStore";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import { useEffect } from "react";
+import "../styles/globals.scss";
+
+export default function ClientWrapper({ children }: { children: React.ReactNode }) {
+  const { setUser, user, logout } = useUserStore();
+  const { data, loading } = useAuth();
+  const error = useErrorStore((s) => s.message);
+  const setError = useErrorStore((s) => s.setError);
+
+  useEffect(() => {
+    if (data) setUser(data);
+  }, [data]);
+
+  const handleRetry = () => {
+    setError(null);
+    window.location.reload();
+  };
+
+  if (loading)
+    return (
+      <div className="app-container container flex flexCenter">
+        <Loader />
+      </div>
+    );
+  if (error) return <RootError message={error} onRetry={handleRetry} />;
+
+  return (
+    <div className="app-container">
+      <ErrorBoundary>
+        <div className="app-container">
+          <Header
+            isAuth={Boolean(user)}
+            name={`${user?.firstName} ${user?.lastName}`}
+            logout={logout}
+          />
+          <main>{children}</main>
+          <Footer email={user?.email} />
+        </div>
+      </ErrorBoundary>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,56 +1,10 @@
-"use client";
-
-import Header from "@/components/Header/Header";
-import "../styles/globals.scss";
-import { useEffect } from "react";
-import { useUserStore } from "../store/useUserStore";
-import Footer from "@/components/Footer/Footer";
-import useAuth from "@/hooks/useAuth";
-import Loader from "@/components/Loader/Loader";
-import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary";
-import { useErrorStore } from "@/store/useErrorStore";
-import RootError from "@/components/RootError/RootError";
+import ClientWrapper from "./ClientWrapper";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const { setUser, user, logout } = useUserStore((state) => state);
-  const { data, loading } = useAuth();
-  const error = useErrorStore((s) => s.message);
-  const setError = useErrorStore((s) => s.setError);
-
-  useEffect(() => {
-    if (data) {
-      setUser(data);
-    }
-  }, [data]);
-
-  const handleRetry = () => {
-    setError(null);
-    window.location.reload();
-  };
-
-  if (loading)
-    return (
-      <div className="app-container container flex flexCenter">
-        <Loader />
-      </div>
-    );
-
-  if (error) return <RootError message={error} onRetry={handleRetry} />;
-
   return (
     <html lang="en">
       <body>
-        <div className="app-container">
-          <ErrorBoundary>
-            <Header
-              isAuth={Boolean(user)}
-              name={`${user?.firstName} ${user?.lastName}`}
-              logout={logout}
-            />
-            <main className="main">{children}</main>
-            <Footer email={user?.email} />
-          </ErrorBoundary>
-        </div>
+        <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
   );
